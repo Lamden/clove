5daf5a2938f7282047513e727905a0de9f4b1394
Tomasz Karbownicki <tomasz.karbownicki@stxnext.com>
2018-01-17 16:10:04 +0100

    [#3] configuration for tox (pytest, isort, flake8) and travis

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup, find_packages
 from codecs import open
 from os import path
+
+from setuptools import find_packages, setup
 
 __version__ = '0.0.1'
 
@@ -10,18 +11,27 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
-
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
-
 setup(
     name='clove',
     version=__version__,
     description='Clove is a library that makes atomic swaps between chains easy.',
     long_description=long_description,
+    install_requires=[
+        'bitcoinlib==0.3.29',
+    ],
+    extras_require={
+        'testing': [
+            'tox==2.9.1',
+            'pytest==3.3.2',
+            'isort==4.2.15',
+            'flake8==3.5.0',
+        ],
+        'docs': [
+            'Sphinx==1.6.6',
+            'sphinx-rtd-theme==0.2.4',
+            'recommonmark==0.4.0',
+        ],
+    },
     url='https://github.com/Landen/clove',
     download_url='https://github.com/Landen/clove/tarball/' + __version__,
     license='GPLv3',
@@ -34,7 +44,5 @@ setup(
     packages=find_packages(exclude=['docs', 'tests*']),
     include_package_data=True,
     author='Lamden Team',
-    install_requires=install_requires,
-    dependency_links=dependency_links,
     author_email='team@landen.io'
 )
