8fa4c9f045e1691d0f43fcab68d87a7ece788256
Tomasz Karbownicki <tomasz.karbownicki@stxnext.com>
2018-02-01 15:00:59 +0100

    [#25] showing transaction details

--- tests/test_bitcoin_transfer.py
+++ tests/test_bitcoin_transfer.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from clove.network.bitcoin import BitcoinTestNet, BitcoinTransaction
 
 
@@ -15,23 +17,64 @@ def test_swap_contract(alice_wallet, bob_wallet):
     assert transaction.contract.is_valid()
 
 
-def test_initiate_atomic_swap(alice_wallet, bob_wallet):
+def test_initiate_atomic_swap(alice_wallet, bob_wallet, alice_outpoints):
     btc_network = BitcoinTestNet()
-    outpoints = [
-        {
-            'txid': '6ecd66d88b1a976cde70ebbef1909edec5db80cff9b8b97024ea3805dbe28ab8',
-            'vout': 1,
-            'value': 0.78956946,
-            'scriptPubKey': '76a914812ff3e5afea281eb3dd7fce9b077e4ec6fba08b88ac'
-        },
-    ]
     transaction = btc_network.initiate_atomic_swap(
         alice_wallet.get_address(),
         bob_wallet.get_address(),
         0.00001,
-        outpoints
+        alice_outpoints
     )
     first_script_signature = transaction.tx.vin[0].scriptSig
     transaction.sign(alice_wallet)
     second_first_script_signature = transaction.tx.vin[0].scriptSig
     assert first_script_signature != second_first_script_signature
+
+
+def test_show_details(alice_wallet, bob_wallet, alice_outpoints):
+    btc_network = BitcoinTestNet()
+    transaction_value = 0.00001
+    transaction = btc_network.initiate_atomic_swap(
+        alice_wallet.get_address(),
+        bob_wallet.get_address(),
+        transaction_value,
+        alice_outpoints
+    )
+    details = transaction.show_details()
+
+    assert type(details) is dict
+
+    str_fields = (
+        'contract',
+        'contract_address',
+        'contract_transaction',
+        'contract_transaction_hash',
+        'recipient_address',
+        'refund_address',
+        'secret',
+        'secret_hash',
+        'value_text',
+    )
+
+    date_fields = (
+        'locktime',
+    )
+
+    float_fields = (
+        'value',
+    )
+
+    for field in str_fields:
+        assert type(details[field]) is str, 'not a string'
+        assert len(details[field]), 'empty string'
+
+    for field in date_fields:
+        assert type(details[field]) is datetime, 'not a datetime'
+
+    for field in float_fields:
+        assert type(details[field]) is float, 'not a float'
+
+    assert details['value'] == transaction_value
+    assert details['value_text'] == '0.00001000 BTC'
+
+    assert sorted(details.keys()) == sorted(str_fields + date_fields + float_fields)
