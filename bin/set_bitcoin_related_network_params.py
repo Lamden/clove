f797bd618cd6a017576845b2c85a06110a54652a
Paweł Ostrowski <oczomb@gmail.com>
2018-02-14 18:11:52 +0100

    [#61] Add support for chains without chainparams.

--- bin/set_bitcoin_related_network_params.py
+++ bin/set_bitcoin_related_network_params.py
@@ -3,6 +3,7 @@
 import glob
 import os
 import re
+from urllib.error import HTTPError, URLError
 import urllib.request
 
 base_pattern = (
@@ -16,14 +17,36 @@ base_pattern = (
 )
 message_start_params = ('pchMessageStart', 'netMagic')
 
-main_patterns = [
+chainparams_patterns = [
     re.compile('CMainParams.*?' + base_pattern.format(message_start=message_start), re.S)
     for message_start in message_start_params
 ]
-test_patterns = [
+chainparams_test_patterns = [
     re.compile('CTestNetParams.*?' + base_pattern.format(message_start=message_start), re.S)
     for message_start in message_start_params
 ]
+
+main_message_start_pattern = re.compile('pchMessageStart\[4\] = {(.*?),(.*?),(.*?),(.*?)};', re.S)
+main_message_start_test_pattern = re.compile(
+    'if (fTestNet).*?'
+    'pchMessageStart\[0\] = (.*?);.*?'
+    'pchMessageStart\[1\] = (.*?);.*?'
+    'pchMessageStart\[2\] = (.*?);.*?'
+    'pchMessageStart\[3\] = (.*?);.*?',
+    re.S
+)
+base58_pattern = re.compile(
+    'class CBitcoinAddress.*?'
+    'PUBKEY_ADDRESS = (.*?),.*?'
+    'SCRIPT_ADDRESS = (.*?),',
+    re.S
+)
+base58_test_pattern = re.compile(
+    'class CBitcoinAddress.*?'
+    'PUBKEY_ADDRESS_TEST = (.*?),.*?'
+    'SCRIPT_ADDRESS_TEST = (.*?),',
+    re.S
+)
 
 
 def add_param_lines(lines, index, testnet=False):
@@ -32,6 +55,9 @@ def add_param_lines(lines, index, testnet=False):
 
 
 def set_params(lines, index, testnet=False):
+    message_start = None
+    base58_prefixes = None
+
     github_line_offset = next((i for i, line in enumerate(lines[index:]) if 'github.com' in line), None)
     port_line_offset = next((i for i, line in enumerate(lines[index:]) if 'port =' in line), None)
 
@@ -44,22 +70,35 @@ def set_params(lines, index, testnet=False):
 
     if len(lines) == port_line_index + 1 or not lines[port_line_index + 1].strip():
         github_link = create_github_link(github_line)
-        if 'chainparams.cpp' not in github_link:
+        if 'chainparams.cpp' in github_link:
+            with urllib.request.urlopen(github_link) as url:
+                if url.status != 200:
+                    return
+                data = url.read().decode()
+
+                message_start, base58_prefixes = find_params_in_chainparams_file(data, testnet=testnet)
+
+        elif 'net.cpp' in github_link:
+            main_github_link = github_link.replace('net.cpp', 'main.cpp')
+            with urllib.request.urlopen(main_github_link) as url:
+                if url.status != 200:
+                    return
+                data = url.read().decode()
+                message_start = find_message_start_in_main_file(data, testnet=testnet)
+
+            base58_github_link = github_link.replace('net.cpp', 'base58.h')
+            with urllib.request.urlopen(base58_github_link) as url:
+                if url.status != 200:
+                    return
+                data = url.read().decode()
+                base58_prefixes = find_base58_prefixes_in_base58_file(data, testnet=testnet)
+
+        if not message_start or not base58_prefixes:
             return
 
-        with urllib.request.urlopen(github_link) as url:
-            if url.status != 200:
-                return
-            data = url.read().decode()
-
-            message_start, base58_prefixes = find_params(data, testnet=testnet)
-            if not message_start or not base58_prefixes:
-                return
-
-            indent = len(port_line) - len(port_line.lstrip(' '))
-            param_lines = [indent * ' ' + line + '\n' for line in [message_start] + base58_prefixes]
-
-            return lines[:port_line_index + 1] + param_lines + lines[port_line_index + 1:]
+        indent = len(port_line) - len(port_line.lstrip(' '))
+        param_lines = [indent * ' ' + line + '\n' for line in [message_start] + base58_prefixes]
+        return lines[:port_line_index + 1] + param_lines + lines[port_line_index + 1:]
 
 
 def create_github_link(line):
@@ -71,18 +110,45 @@ def create_github_link(line):
     return github_base + github_link
 
 
-def find_params(data, testnet=False):
+def find_message_start_in_main_file(data, testnet=False):
+    if testnet:
+        search = main_message_start_test_pattern.search(data)
+    else:
+        search = main_message_start_pattern.search(data)
+
+    if search:
+        strings = [s.strip() for s in search.groups()]
+        return extract_message_start(strings)
+
+
+def find_base58_prefixes_in_base58_file(data, testnet=False):
+    if testnet:
+        search = base58_test_pattern.search(data)
+    else:
+        search = base58_pattern.search(data)
+
+    if search:
+        strings = [s.strip() for s in search.groups()]
+        try:
+            values = list(map(int, strings))
+            values.append(values[0] + 128)
+            return get_prefixes_lines(values)
+        except ValueError:
+            return
+
+
+def find_params_in_chainparams_file(data, testnet=False):
     message_start = None
     base58_prefixes = None
     search = None
 
     if testnet:
-        for pattern in test_patterns:
+        for pattern in chainparams_test_patterns:
             search = pattern.search(data)
             if search:
                 break
     else:
-        for pattern in main_patterns:
+        for pattern in chainparams_patterns:
             search = pattern.search(data)
             if search:
                 break
@@ -121,6 +187,10 @@ def extract_base58_prefixes(strings):
         except ValueError:
             return
 
+    return get_prefixes_lines(prefixes)
+
+
+def get_prefixes_lines(prefixes):
     return [
         "base58_prefixes = {",
         f"    'PUBKEY_ADDR': {prefixes[0]},",
@@ -162,11 +232,14 @@ if __name__ == '__main__':
             indices = [i for i, line in enumerate(file_lines) if 'Class with all the necessary' in line]
             if not indices:
                 continue
+            try:
+                if len(indices) == 2:
+                    file_lines = add_param_lines(file_lines, indices[1], testnet=True)
 
-            if len(indices) == 2:
-                file_lines = add_param_lines(file_lines, indices[1], testnet=True)
+                file_lines = add_param_lines(file_lines, indices[0])
+            except(HTTPError, URLError):
+                continue
 
-            file_lines = add_param_lines(file_lines, indices[0])
             if len(file_lines) == file_length:
                 continue
 
